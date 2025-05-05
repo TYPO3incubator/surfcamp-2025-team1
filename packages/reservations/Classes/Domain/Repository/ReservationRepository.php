c2392475dc13ccd6a02fff574a60b33aec788744
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2025-05-05 22:23:40 +0200

    fix(reservations-extension): fix typo

--- packages/reservations/Classes/Domain/Repository/ReservationRepository.php
+++ packages/reservations/Classes/Domain/Repository/ReservationRepository.php
@@ -3,8 +3,8 @@
 namespace TYPO3Incubator\Reservations\Domain\Repository;
 
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use TYPO3\CMS\Extbase\Persistence\Repository;
 
-/**
 class ReservationRepository extends Repository
 {
     /**
