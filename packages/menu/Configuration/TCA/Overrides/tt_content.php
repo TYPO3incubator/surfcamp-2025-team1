7842705c2fc71138dd6d524b6c78a1f584c94af5
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2025-05-06 19:04:49 +0200

    task: optimizations
    Refs: #4, #11

--- packages/menu/Configuration/TCA/Overrides/tt_content.php
+++ packages/menu/Configuration/TCA/Overrides/tt_content.php
@@ -7,11 +7,8 @@ defined('TYPO3') or die();
 
 (static function (): void {
     $pluginKey = ExtensionUtility::registerPlugin(
-        // extension name, matching the PHP namespaces (but without the vendor)
         'Menu',
-        // arbitrary, but unique plugin name (not visible in the backend)
         'ListMenu',
-        // plugin title, as visible in the drop-down in the backend, use "LLL:" for localization
         'LLL:EXT:menu/Resources/Private/Language/locallang_be.xlf:plugin.menu',
     );
 
