621a3ca3d31a54c5d62c113e2c7eb0f15b04927e
ekkiheiti <paul@stoske.eu>
2025-05-06 10:43:32 +0100

    fix: removed unused controller methods Refs: #4, #11

--- packages/menu/Classes/Controller/DishController.php
+++ packages/menu/Classes/Controller/DishController.php
@@ -3,11 +3,7 @@
 namespace TYPO3Incubator\Menu\Controller;
 
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
-use TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException;
-use TYPO3Incubator\Menu\Domain\Model\Dish;
 use TYPO3Incubator\Menu\Domain\Repository\DishRepository;
 
 class DishController extends ActionController
@@ -18,43 +14,9 @@ class DishController extends ActionController
     {
     }
 
-    public function showAction(Dish $dish): ResponseInterface
-    {
-        $this->view->assign('dish', $dish);
-        return $this->htmlResponse();
-    }
-
     public function listAction(): ResponseInterface
     {
         $this->view->assign('dishes', $this->dishRepository->findAll());
         return $this->htmlResponse();
-    }
-
-    /**
-     * @throws IllegalObjectTypeException
-     */
-    public function createAction(Dish $dish): ResponseInterface
-    {
-        $this->dishRepository->add($dish);
-        return $this->redirect('list');
-    }
-
-    /**
-     * @throws IllegalObjectTypeException
-     */
-    public function deleteAction(Dish $dish): ResponseInterface
-    {
-        $this->dishRepository->remove($dish);
-        return $this->redirect('list');
-    }
-
-    /**
-     * @throws UnknownObjectException
-     * @throws IllegalObjectTypeException
-     */
-    public function updateAction(Dish $dish): ResponseInterface
-    {
-        $this->dishRepository->update($dish);
-        return $this->redirect('list');
     }
 }
--- packages/menu/Classes/Controller/DrinkController.php
+++ packages/menu/Classes/Controller/DrinkController.php
@@ -4,9 +4,6 @@ namespace TYPO3Incubator\Menu\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
-use TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException;
-use TYPO3Incubator\Menu\Domain\Model\Drink;
 use TYPO3Incubator\Menu\Domain\Repository\DrinkRepository;
 
 class DrinkController extends ActionController
@@ -17,43 +14,9 @@ class DrinkController extends ActionController
     {
     }
 
-    public function showAction(Drink $drink): ResponseInterface
-    {
-        $this->view->assign('drink', $drink);
-        return $this->htmlResponse();
-    }
-
     public function listAction(): ResponseInterface
     {
         $this->view->assign('drinks', $this->drinkRepository->findAll());
         return $this->htmlResponse();
-    }
-
-    /**
-     * @throws IllegalObjectTypeException
-     */
-    public function createAction(Drink $drink): ResponseInterface
-    {
-        $this->drinkRepository->add($drink);
-        return $this->redirect('list');
-    }
-
-    /**
-     * @throws IllegalObjectTypeException
-     */
-    public function deleteAction(Drink $drink): ResponseInterface
-    {
-        $this->drinkRepository->remove($drink);
-        return $this->redirect('list');
-    }
-
-    /**
-     * @throws UnknownObjectException
-     * @throws IllegalObjectTypeException
-     */
-    public function updateAction(Drink $drink): ResponseInterface
-    {
-        $this->drinkRepository->update($drink);
-        return $this->redirect('list');
     }
 }
