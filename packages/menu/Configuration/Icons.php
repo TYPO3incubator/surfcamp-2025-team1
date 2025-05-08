394fb1187758bff157c0cc93bcf9a6f0dcb3eb2b
Paul Stoske <paul@stoske.eu>
2025-05-08 22:29:12 +0100

    feat: add icon for menu plugin
    Refs: #39

---
+++ packages/menu/Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'tx-menu-svgicon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:menu/Resources/Public/Icons/restaurant-fine.svg',
+    ],
+];
