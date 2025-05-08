3c11ce8cd57bb64f7558705eb91c136aac9e9117
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2025-05-08 18:35:06 +0200

    task(reservations-extension): optimized TypoScript
    Refs: #14

--- packages/reservations/ext_localconf.php
+++ packages/reservations/ext_localconf.php
@@ -3,20 +3,12 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3Incubator\Reservations\Controller\ReservationController;
 
 defined('TYPO3') or die();
 
 ExtensionManagementUtility::addTypoScriptSetup(
     trim('
         module.tx_form {
-            settings {
-                yamlConfigurations {
-                    120 = EXT:reservations/Configuration/Form/Setup.yaml
-                }
-            }
-        }
-        plugin.tx_form {
             settings {
                 yamlConfigurations {
                     120 = EXT:reservations/Configuration/Form/Setup.yaml
