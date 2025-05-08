47de780247a7e58f1636ef8c4bca1fb687cd047a
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2025-05-08 17:25:56 +0200

    feat(reservations-extension): introduced reservation form
    Refs: #14

--- packages/reservations/ext_localconf.php
+++ packages/reservations/ext_localconf.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use TYPO3Incubator\Reservations\Controller\ReservationController;
 
@@ -13,4 +14,23 @@ ExtensionUtility::configurePlugin(
     [ReservationController::class => 'form'],
     [],
     ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+);
+
+ExtensionManagementUtility::addTypoScriptSetup(
+    trim('
+        module.tx_form {
+            settings {
+                yamlConfigurations {
+                    120 = EXT:reservations/Configuration/Form/Setup.yaml
+                }
+            }
+        }
+        plugin.tx_form {
+            settings {
+                yamlConfigurations {
+                    120 = EXT:reservations/Configuration/Form/Setup.yaml
+                }
+            }
+        }
+    ')
 );
