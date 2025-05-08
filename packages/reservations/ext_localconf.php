8964446de86bf4d2ceb5bee8d7706dd40d8d02db
Paul Stoske <paul@stoske.eu>
2025-05-09 00:23:35 +0100

    feat: create hook for sending confirmation mails
    Refs: #34

--- packages/reservations/ext_localconf.php
+++ packages/reservations/ext_localconf.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3Incubator\Reservations\Hooks\DataHandlerHook;
 
 defined('TYPO3') or die();
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = DataHandlerHook::class;
 
 ExtensionManagementUtility::addTypoScriptSetup(
     trim('
