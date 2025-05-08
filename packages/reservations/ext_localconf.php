df90d8a0725fe8d3fc025255bf40ba378f03e244
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2025-05-08 17:39:33 +0200

    task(reservations-extension): removed unnecessary plugin registration (now using forms instead)
    Refs: #14

--- packages/reservations/ext_localconf.php
+++ packages/reservations/ext_localconf.php
@@ -3,18 +3,9 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use TYPO3Incubator\Reservations\Controller\ReservationController;
 
 defined('TYPO3') or die();
-
-ExtensionUtility::configurePlugin(
-    'Reservations',
-    'ReservationForm',
-    [ReservationController::class => 'form'],
-    [],
-    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-);
 
 ExtensionManagementUtility::addTypoScriptSetup(
     trim('
